aba68fa3a66bd5f87a399aff0c44499e37062632
cocoritzy <lilounina.nina@gmail.com>
2025-03-03 17:15:37 +0100

    change les couleurs

--- home.py
+++ home.py
@@ -2,62 +2,27 @@ import streamlit as st
 import base64
 from streamlit_option_menu import option_menu
 
-# 1. as sidebar menu
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
-
-# 2. horizontal menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-selected2
-
-# 3. CSS style definitions
-selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
-    }
-)
-
-# 4. Manual item selection
-if st.session_state.get('switch_button', False):
-    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
-    manual_select = st.session_state['menu_option']
-else:
-    manual_select = None
-    
-selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    orientation="horizontal", manual_select=manual_select, key='menu_4')
-st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
-selected4
-
-# 5. Add on_change callback
-def on_change(key):
-    selection = st.session_state[key]
-    st.write(f"Selection changed to {selection}")
-    
-selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
-                        icons=['house', 'cloud-upload', "list-task", 'gear'],
-                        on_change=on_change, key='menu_5', orientation="horizontal")
-selected5
-
 def home():
-
     # Page configs (tab title, favicon)
     st.set_page_config(
         page_title="Coline Ritz's Portfolio",
         page_icon="🍕",
     )
 
-    # CSS styles file
+    page = st.sidebar.selectbox("Select a Page", ["About me", "My projects"])
+
+    # First Page
+    if page == "About me":
+        homepage()
+
+    # Second Page
+    elif page == "My projects":
+        analyseddata()
+
+
+def homepage():
+
+    # CSS styles file (if any)
     with open("styles/main.css") as f:
         st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
@@ -69,7 +34,6 @@ def home():
     with open("assets/CV (1).pdf", "rb") as pdf_file:
         pdf_bytes = pdf_file.read()
 
-    
     # Top title
     st.write(f"""<div class="title"><strong>Hi! My name is</strong> Coline Ritz👋</div>""", unsafe_allow_html=True)
 
@@ -86,22 +50,13 @@ def home():
             </div>
         </div>
     </div>
-    """, 
-    unsafe_allow_html=True)
-
-    # Alternative image (static and rounded) uncomment it if you prefer this one
-    # st.write(f"""
-    # <div style="display: flex; justify-content: center;">
-    #    <img src="{img}" alt="Coline Ritz" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
-    # </div>
-    # """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
     # Subtitle
     st.write(f"""<div class="subtitle" style="text-align: center;">Design Engineer</div>""", unsafe_allow_html=True)
 
     # Social Icons
     social_icons_data = {
-        # Platform: [URL, Icon]
         "LinkedIn": ["https://www.linkedin.com/in/e-domingo", "https://cdn-icons-png.flaticon.com/512/174/174857.png"],
         "GitHub": ["https://github.com/enricd", "https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg"]
     }
@@ -111,8 +66,7 @@ def home():
     st.write(f"""
     <div style="display: flex; justify-content: center; margin-bottom: 20px;">
         {''.join(social_icons_html)}
-    </div>""", 
-    unsafe_allow_html=True)
+    </div>""", unsafe_allow_html=True)
 
     st.write("##")
 
@@ -125,7 +79,7 @@ def home():
 
     - ❤️ I am passionate about **Machine Learning/Deep Learning, MLOps, Data, Software Engineering, Computer Vision, Bioinformatics, UAVs, Optimization, Automation**, and more!
     
-    - 🤖 I enojoy developing projects such as [SpeedClimbing.AI](https://www.instagram.com/speedclimbing.ai) (🏗️under construction) and participating at platforms like [Kaggle](https://www.kaggle.com/edomingo) 📈
+    - 🤖 I enjoy developing projects such as [SpeedClimbing.AI](https://www.instagram.com/speedclimbing.ai) (🏗️under construction) and participating at platforms like [Kaggle](https://www.kaggle.com/edomingo) 📈
     
     - 🏂 Also practicing sports such as snowboard, wakeboard and climbing 🧗
     
@@ -144,11 +98,27 @@ def home():
         mime="application/pdf",
     )
 
-    st.write("##")
-    
-    st.write(f"""<div class="subtitle" style="text-align: center;">⬅️ Check out my Projects in the navigation menu! (Coming soon...)</div>""", unsafe_allow_html=True)
+
+def analyseddata():
+
+    # CSS styles file (if any)
+    with open("styles/main.css") as f:
+        st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        
+    projects = {
+        "Project 1: ML-based SpeedClimbing": "Description of the project...",
+        "Project 2: UAS Development": "Another description here...",
+        "Project 3: Kaggle Data Science Challenge": "Details of this Kaggle competition...",
+        "Project 4: MLOps Pharma Project": "Info about a pharma project...",
+    }
+
+    # Create the dropdown menu for project selection
+    selected_project = st.selectbox("Select a Project", list(projects.keys()))
+
+    # Display selected project details
+    st.subheader(f"Details about {selected_project}")
+    st.write(projects[selected_project])
 
 
 if __name__ == "__main__":
-
     home()
